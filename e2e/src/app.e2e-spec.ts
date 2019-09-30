1a5df0e555ffb70749249c29e6fe6259f3b6bf32
Jesse Schneider <jessewschneider96@gmail.com>
2019-10-01 00:48:31 +1000

    Added 1 e2e test, and fixed a dep issue with popper.js and bootstrap that was causing an unnecessary error

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,17 +1,20 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('MEANChat App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Meanchat', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to MEAN-chat!');
+    expect(page.getTitleText()).toEqual('Login to MEANChat!');
   });
+
+
+  //insert other e2e it cases here, minimum 10
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
